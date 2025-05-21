8e3f0c2653c036336d434c5ac3488a8fe9efd221
Niklas <niklas.herrmann@std.dhsh.de>
2025-05-21 12:40:07 +0200

    changing text components on help&Support page

--- frontend/src/pages/legal/HelpSupport.tsx
+++ frontend/src/pages/legal/HelpSupport.tsx
@@ -249,7 +249,7 @@ const HelpSupport = () => {
                   <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M9.663 17h4.673M12 3v1m6.364 1.636l-.707.707M21 12h-1M4 12H3m3.343-5.657l-.707-.707m2.828 9.9a5 5 0 117.072 0l-.548.547A3.374 3.374 0 0014 18.469V19a2 2 0 11-4 0v-.531c0-.895-.356-1.754-.988-2.386l-.548-.547z" />
                 </svg>
                 <h3 className="font-medium mb-1">Tipps & Tricks</h3>
-                <Text className="text-sm text-gray-500 mb-3">Nützliche Hinweise für fortgeschrittene Benutzer.</Text>
+                <Text className="text-sm text-gray-500 mb-3">Nützliche Hinweise für neue Benutzer.</Text>
                 <a href="#haeufigGestellteFragen" className="text-blue-600 text-sm font-medium hover:underline">Artikel lesen</a>
               </div>
               
